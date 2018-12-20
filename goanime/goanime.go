bdce1a5fbf7580be4cf1eb4865f20605898bf99f
jafriyie1 <sfjafthemp@gmail.com>
2018-12-19 20:23:27 -0600

    changing file structure

--- goanime/goanime.go
+++ goanime/goanime.go
@@ -1,7 +1,7 @@
 // This program allows one to watch anime
 // without having to go through the hoops
 // of using popular anime sites
-package main
+package goanime
 
 import (
 	"bufio"
@@ -14,12 +14,14 @@ import (
 	"runtime"
 	"strconv"
 	"strings"
+	"sync"
+	"unicode"
 
-	"github.com/derekparker/trie"
-	"github.com/jafriyie1/animetries"
-
+	"github.com/PuerkitoBio/goquery"
 	"github.com/chromedp/chromedp"
 	"github.com/chromedp/chromedp/runner"
+	"github.com/derekparker/trie"
+	"github.com/jafriyie1/animetries"
 )
 
 func openBrowser(url string) {
@@ -41,48 +43,43 @@ func openBrowser(url string) {
 }
 
 func getShow(b *trie.Trie) string {
-	var line string
+	var show string
 	scanner := bufio.NewScanner(os.Stdin)
 	fmt.Println("Please input anime name.\nYou can also input a part of the show to search.")
 
 	if scanner.Scan() {
-		line = scanner.Text()
+		show = scanner.Text()
 	}
-	fmt.Println("Here are some shows that match your search:cle")
+
+	show = strings.TrimSpace(show)
+
+	upperCase := byte(unicode.ToUpper(rune(show[0])))
+	upperLine := string(upperCase) + show[1:]
+
+	fmt.Println("Here are some shows that match your search:")
 	fmt.Println()
-	animetries.PossibleShows(b, line)
+	animetries.PossibleShows(b, upperLine)
 	fmt.Println()
-	fmt.Println("Please copy or type anime name and hit enter")
+	fmt.Println("Please copy or type the anime show and hit enter")
 	if scanner.Scan() {
-		line = scanner.Text()
+		show = scanner.Text()
 	}
-	return line
+	return show
 
 }
 
-func getURL(line string) (string, string, string) {
-	//var line string
-	var episode string
+func getSeason() string {
 	var season string
-
 	scanner := bufio.NewScanner(os.Stdin)
-	/*
-		fmt.Println("Please input anime name")
-
-		if scanner.Scan() {
-			line = scanner.Text()
-		}
-	*/
-	line = strings.TrimSpace(line)
-	fmt.Println("Please input episode number")
-	if scanner.Scan() {
-		episode = scanner.Text()
-	}
-
 	fmt.Println("Which season would you like to watch (1st, 2nd, 3rd, 4th, etc.)?\n(Hit enter if there is no season) ")
 	if scanner.Scan() {
 		season = scanner.Text()
 	}
+	return season
+}
+
+func getURL(show, episode, season string) (string, string, string) {
+
 	season = strings.TrimSpace(season)
 
 	episodeToInt, err := strconv.ParseInt(episode, 10, 32)
@@ -95,79 +92,184 @@ func getURL(line string) (string, string, string) {
 	if episodeToInt < 10 {
 		episode = "0" + episode
 	}
+	if season == "1st" {
+		season = ""
+	}
 	if season != "" {
 		season = "-" + season + "-Season"
 	}
-	line = strings.Replace(line, ":", "", -1)
-	line = strings.Replace(line, ")", "", -1)
-	line = strings.Replace(line, "(", "", -1)
-	line = strings.Replace(line, " ", "-", -1)
-	base_url := "https://kissanime.ru/Anime/" + line + season + "/"
+	show = strings.Replace(show, ":", "", -1)
+	show = strings.Replace(show, ")", "", -1)
+	show = strings.Replace(show, "(", "", -1)
+	show = strings.Replace(show, " ", "-", -1)
+	base_url := "https://kissanime.ru/Anime/" + show + season + "/"
 
 	episode = "Episode-" + episode
 
-	return line, base_url, episode
+	return show, base_url, episode
+}
+
+func getOneEpisode() string {
+	var episode string
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Println("Please input episode number")
+	if scanner.Scan() {
+		episode = scanner.Text()
+	}
+
+	episode = strings.TrimSpace(episode)
+	return episode
+
+}
+
+func getRangeOfEpisodes() (string, string) {
+	var episodeStart string
+	var episodeEnd string
+	scanner := bufio.NewScanner(os.Stdin)
+
+	if episodeStart == episodeEnd {
+		fmt.Println("Which episode would you like to watch?\nNote: do not place any zeros in front of the episode (i.e. type 25 for Episode 025)")
+		fmt.Println("Please input episode number")
+
+		if scanner.Scan() {
+			episodeStart = scanner.Text()
+		}
+		episodeStart = strings.TrimSpace(episodeStart)
+		episodeEnd = episodeStart
+
+	} else {
+		fmt.Println("What range of episodes would you like to watch?")
+		fmt.Println("Please input starting episode number")
+		if scanner.Scan() {
+			episodeStart = scanner.Text()
+		}
+		fmt.Println("Please input ending episode number")
+		if scanner.Scan() {
+			episodeEnd = scanner.Text()
+		}
+
+		episodeStart = strings.TrimSpace(episodeStart)
+		episodeEnd = strings.TrimSpace(episodeEnd)
+	}
+
+	return episodeStart, episodeEnd
 }
 
 func click(url string, val *string) chromedp.Tasks {
 	return chromedp.Tasks{
 		chromedp.Navigate(url),
+		//chromedp.WaitVisible(`#footer`),
 		chromedp.Click(`a.specialButton`, chromedp.NodeVisible),
+		chromedp.Click(`a.specialButton`, chromedp.NodeVisible),
+		chromedp.WaitVisible(`div#divMyVideo`),
 		chromedp.OuterHTML(`iframe#my_video_1`, val, chromedp.NodeVisible),
 	}
 }
 
-func main() {
-	_, builtTrie, animeMap := animetries.BuildAnimeTrie()
+func clickForEpisodeList(url string, val *string) chromedp.Tasks {
+	return chromedp.Tasks{
+		chromedp.Navigate(url),
+		chromedp.WaitVisible(`#head`),
+		chromedp.OuterHTML(`table.listing`, val, chromedp.NodeVisible),
+	}
+}
 
-	searchedShow := getShow(builtTrie)
-	maxEpisode := animetries.GetEpisodeFromMap(searchedShow, animeMap)
-	fmt.Println()
-	fmt.Println(searchedShow + " has a maximum (or currently) " + maxEpisode + " episodes")
-	_, base_url, episode := getURL(searchedShow)
-	//fmt.Println(base_url)
-
-	episode_search := base_url + episode + "?id=&s=rapidVideo"
+func doGoAnime() (*chromedp.CDP, context.Context) {
 
 	// chromedp
-	ctxt, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	var newerr error
-	var val string
-
-	fmt.Println("Please wait....")
+	ctxt, _ := context.WithCancel(context.Background())
 
 	// create headless chrome instance
-	c, newerr := chromedp.New(ctxt, chromedp.WithRunnerOptions(
-		runner.Flag("headless", true),
-		runner.Flag("disable-gpu", true)),
-	)
-	if newerr != nil {
-		log.Fatal(newerr)
-	}
 
+	c, _ := chromedp.New(ctxt, chromedp.WithRunnerOptions(
+
+		runner.Flag("no-sandbox", true),
+		runner.Flag("headless", true),
+		runner.Flag("disable-gpu", true),
+		runner.Flag("no-first-run", true),
+		runner.Flag("no-default-browser-check", true),
+	))
+	log.SetFlags(0)
+
+	return c, ctxt
+
+}
+
+func concurrentEpisodes(lowerLimitEpisode, upperLimitEpisode, searchedShow, season string, wg *sync.WaitGroup, c *chromedp.CDP, ctxt context.Context) {
+
+	var val string
+
+	_, baseURL, episode := getURL(searchedShow, lowerLimitEpisode, season)
+
+	episodeSearch := baseURL + episode + "?id=&s=rapidVideo"
 	// run task list
-	err := c.Run(ctxt, click(episode_search, &val))
+	log.SetFlags(0)
+
+	err := c.Run(ctxt, click(episodeSearch, &val))
 	if err != nil {
 		log.Fatal(err)
 		os.Exit(1)
 	}
 
-	/*
-		fmt.Println("out\n")
-		fmt.Println(val)
-	*/
+	err = c.Shutdown(ctxt)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	r, _ := regexp.Compile(`src="(.*?)"`)
 	rapidVideoString := r.FindAllString(val, 1)
 	urlRapidVideo := rapidVideoString[0][4:]
-	/*
-		fmt.Println(urlRapidVideo)
-		fmt.Println(urlRapidVideo[:5])
-		fmt.Println(reflect.TypeOf(urlRapidVideo))
-	*/
+
 	url := strings.Replace(urlRapidVideo, "\"", "", -1)
 	openBrowser(url)
+	wg.Done()
+
+}
+
+func getEpisodeList(searchedShow, season string) {
+	log.SetFlags(0)
+	var val string
+	_, baseURL, _ := getURL(searchedShow, "1", season)
+
+	ctxt, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	var episodeSlice []string
+
+	c, newerr := chromedp.New(ctxt, chromedp.WithRunnerOptions(
+
+		runner.Flag("headless", true),
+		runner.Flag("disable-gpu", true),
+		runner.Flag("no-first-run", true),
+		runner.Flag("no-default-browser-check", true),
+	))
+
+	if newerr != nil {
+		log.Fatal(newerr)
+	}
+	err := c.Run(ctxt, clickForEpisodeList(baseURL, &val))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	stringVals := strings.NewReader(val)
+	doc, _ := goquery.NewDocumentFromReader(stringVals)
+
+	doc.Find("tbody").Each(func(i int, s *goquery.Selection) {
+		episodeVals := s.Find("a").Text()
+		trimEpisodeVals := strings.Trim(episodeVals, "\n\t\r")
+
+		episodeSlice = append(episodeSlice, trimEpisodeVals)
+	})
+
+	for _, episodes := range episodeSlice {
+		fmt.Println(episodes)
+	}
+
+	cErr := c.Shutdown(ctxt)
+	if cErr != nil {
+		log.Fatal(cErr)
+	}
 
 }
---
+++ goanime/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/jafriyie1/animetries"
+)
+
+func main() {
+	_, builtTrie, _ := animetries.BuildAnimeTrie()
+	var option string
+
+	scanner := bufio.NewScanner(os.Stdin)
+	searchedShow := getShow(builtTrie)
+	searchedShow = strings.TrimSpace(searchedShow)
+
+	fmt.Println()
+	season := getSeason()
+	fmt.Println("Here is a list of episodes for the given show and season (please wait):")
+
+	getEpisodeList(searchedShow, season)
+
+	fmt.Println()
+	time.Sleep(5)
+	fmt.Println("Scroll up to view episodes (and please ignore other messages).\nWould you like to watch one episode or mutliple (1 for episode, 2 for multiple)")
+
+	if scanner.Scan() {
+		option = scanner.Text()
+	}
+	option = strings.TrimSpace(option)
+	var lowerLimitEpisode string
+
+	upperLimitEpisode := " "
+
+	if option == "1" {
+		lowerLimitEpisode, lowerLimitEpisode = getRangeOfEpisodes()
+	} else {
+		fmt.Println("WARNING: You can only get a maximum of 2 episodes.\nOutside of that you will get wonky behavior.")
+
+		lowerLimitEpisode, upperLimitEpisode = getRangeOfEpisodes()
+	}
+	upperLimitEpisode = lowerLimitEpisode
+
+	wg := new(sync.WaitGroup)
+
+	lowerEpisode, _ := strconv.Atoi(lowerLimitEpisode)
+	upperEpisode, _ := strconv.Atoi(upperLimitEpisode)
+
+	c, ctxt := doGoAnime()
+	fmt.Println("Please wait....")
+	for i := lowerEpisode; i < upperEpisode+1; i++ {
+		wg.Add(1)
+		loopedEpisode := strconv.Itoa(i)
+
+		go concurrentEpisodes(loopedEpisode, upperLimitEpisode, searchedShow, season, wg, c, ctxt)
+
+	}
+	wg.Wait()
+	cErr := c.Shutdown(ctxt)
+
+	if cErr != nil {
+		log.Fatal(cErr)
+	}
+
+}
