9dd2955379c9d55bee1c89865d9d425bcaf77979
jafriyie1 <joel.o.afriyie@gmail.com>
2019-06-20 22:20:56 -0500

    Change to error message

--- animescrapper/animescrapper.go
+++ animescrapper/animescrapper.go
@@ -210,7 +210,7 @@ func DoGoAnime() (context.Context, context.CancelFunc) {
 
 }
 
-func OpenEpisodesAPI(ctxt Context, cancel.lowerLimitEpisode string, searchedShow string) string {
+func OpenEpisodesAPI(lowerLimitEpisode string, searchedShow string) string {
 
 	ctxt, cancel := DoGoAnime()
 	defer cancel()
